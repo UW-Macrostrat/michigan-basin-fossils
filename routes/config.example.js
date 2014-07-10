a60907d4b70f14596d080c5a3d288064d40db061
John J Czaplewski <jczaplew@gmail.com>
2014-07-10 14:36:51 -0500

    Cleaned up example config

--- routes/config.example.js
+++ routes/config.example.js
@@ -1,11 +1,9 @@
 module.exports = {
-  //"imagePath": "/Library/WebServer/Documents/mibasin/images/",
   "imagePath": "/Users/you/Sites/ummp/public/images/",
   "credentials": {
     'host': 'localhost',
     'user': 'you',
     'password': 'yourpassword',
     'database': 'yourdatabase'
-   // 'socketPath': '/var/tmp/mariadb.sock'
   }
 }
